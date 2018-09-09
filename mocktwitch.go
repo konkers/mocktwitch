3f97400118a1d9382d930fd2448ce008c6af3320
Erik Gilling <konkers@konkers.net>
2018-09-08 21:22:13 -0700

    Fix Message format.

--- mocktwitch.go
+++ mocktwitch.go
@@ -52,8 +52,8 @@ func (t *Twitch) Close() {
 }
 
 func (t *Twitch) SendMessage(channel string, author string, message string) {
-	encoded := fmt.Sprintf("@badges=subscriber/6,premium/1;color=#FF0000;display-name=%s;emotes=;id=2a31a9df-d6ff-4840-b211-a2547c7e656e;mod=0;room-id=11148817;subscriber=1;tmi-sent-ts=1490382457309;turbo=0;user-id=78424343;user-type= :redflamingo13!redflamingo13@redflamingo13.tmi.twitch.tv PRIVMSG #%s :%s\r\n",
-		author, channel, message)
+	encoded := fmt.Sprintf("@badges=subscriber/6,premium/1;color=#FF0000;display-name=%s;emotes=;id=2a31a9df-d6ff-4840-b211-a2547c7e656e;mod=0;room-id=11148817;subscriber=1;tmi-sent-ts=1490382457309;turbo=0;user-id=78424343;user-type= :%s!%s@%s.tmi.twitch.tv PRIVMSG #%s :%s\r\n",
+		author, author, author, author, channel, message)
 
 	fmt.Fprint(t.ircConn, encoded)
 }
